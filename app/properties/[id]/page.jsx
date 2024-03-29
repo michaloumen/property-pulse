1c896fc2c744a25f6774abc7dbf52e477f58cd79
michaloumen <michelle.mendonca@minu.co>
2024-03-29 18:08:40 -0300

    use client not working

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,18 @@
+'use client';
+import { useRouter } from 'next/navigation'
+
 const PropertyPage = () => {
+  const router = useRouter()
+
   return (
-    <div>PropertyPage</div>
+    <div>
+      <button
+        onClick={() => router.push('/')}
+        className='bg-blue-500 p-2'
+      >
+        Go Home
+      </button>
+    </div >
   )
 }
 
