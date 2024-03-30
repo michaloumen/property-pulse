557e0d6c5c5bbaf9c1bec15be8adff0a23caf1b2
michaloumen <michelle.mendonca@minu.co>
2024-03-30 20:16:43 -0300

    HomeProperties component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -4,11 +4,11 @@ import PropertyCard from '../../components/PropertyCard';
 const PropertiesPage = () => {
   return (
     <div>
-      <section class='px-4 py-6'>
+      <section className='px-4 py-6'>
         {properties.length === 0 ? (
           <p>No properties found</p>
         ) : (
-          <div class='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
