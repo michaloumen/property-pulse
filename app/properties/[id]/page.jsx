f87a43892b03ccad4570f17685ace6f30950c6ec
michaloumen <michelle.mendonca@minu.co>
2024-04-03 11:07:56 -0300

    spinner component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { fetchProperty } from '../../../utils/requests';
+import { FaArrowLeft } from 'react-icons/fa';
 import PropertyHeaderImage from '../../../components/PropertyHeaderImage';
 import PropertyDetails from '../../../components/PropertyDetails';
-import { FaArrowLeft } from 'react-icons/fa';
+import Spinner from '../../../components/Spinner';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -41,6 +42,7 @@ const PropertyPage = () => {
 
   return (
     <>
+      {loading && <Spinner loading={loading} />}
       {!loading && property && (
         <>
           <PropertyHeaderImage image={property.images[0]} />
@@ -58,7 +60,7 @@ const PropertyPage = () => {
           <section className="bg-blue-50">
             <div className="container m-auto py-10 px-6">
               <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                <PropertyDetails property={property}/>
+                <PropertyDetails property={property} />
                 <aside className="space-y-4">
                   <button
                     className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
