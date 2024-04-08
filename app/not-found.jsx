7d757e4d4766c1f287b7990ea29f4647d072c5e2
michaloumen <michelle.mendonca@minu.co>
2024-04-07 22:28:18 -0300

    sign in button

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,29 +3,29 @@ import { FaExclamationTriangle } from 'react-icons/fa';
 
 const NotFoundPage = () => {
   return (
-    <section class='bg-blue-50 min-h-screen flex-grow'>
-      <div class='container m-auto max-w-2xl py-24'>
+    <section className='bg-blue-50 min-h-screen flex-grow'>
+      <div className='container m-auto max-w-2xl py-24'>
         <div
-          class='bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0'
+          className='bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0'
         >
-          <div class='flex justify-center'>
+          <div className='flex justify-center'>
             <FaExclamationTriangle
-              class='fas fa-exclamation-triangle fa-5x text-8xl text-yellow-400'
+              className='fas fa-exclamation-triangle fa-5x text-8xl text-yellow-400'
             ></FaExclamationTriangle>
           </div>
-          <div class='text-center'>
-            <h1 class='text-3xl font-bold mt-4 mb-2'>Page Not Found</h1>
-            <p class='text-gray-500 text-xl mb-10'>
+          <div className='text-center'>
+            <h1 className='text-3xl font-bold mt-4 mb-2'>Page Not Found</h1>
+            <p className='text-gray-500 text-xl mb-10'>
               The page you are looking for does not exist.
             </p>
             <Link
               href='/'
-              class='bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded'
+              className='bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded'
             >Go Home</Link>
           </div>
         </div>
       </div>
-      <div class='flex-grow'></div>
+      <div className='flex-grow'></div>
     </section>
   )
 }
--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -2,9 +2,6 @@
 import { SessionProvider } from 'next-auth/react';
 
 const AuthProvider = ({ children }) => {
-  return (
-    <SessionProvider>{children}</SessionProvider>
-  )
+  return <SessionProvider>{children}</SessionProvider>;
 };
-
 export default AuthProvider;
