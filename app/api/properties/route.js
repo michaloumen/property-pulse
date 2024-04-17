881966f7d9ef4d80c9e7fd5e30bf0779d46afe8c
michaloumen <michelle.mendonca@minu.co>
2024-04-16 22:30:06 -0300

    cloudinary with error

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from '../../../config/database';
 import Property from '../../../models/Property';
 import { getSessionUser } from '../../../utils/getSessionUser';
+import cloudinary from '../../../config/cloudinary';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -61,9 +62,37 @@ export const POST = async (request) => {
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
       },
-      owner: userId,
-      // images
+      owner: userId
     };
+
+    // Upload image(s) to Cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // Convert the image data to base64
+      const imageBase64 = imageData.toString('base64');
+
+      // Make request to upload to Cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: 'propertypulse',
+          secure: true,
+          rejectUnauthorized: false
+        }
+      );
+
+      imageUploadPromises.push(result.secure_url);
+
+      // Wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+      // Add uploaded images to the propertyData object
+      propertyData.images = uploadedImages;
+    }
 
     const newProperty = new Property(propertyData);
     await newProperty.save();
@@ -73,6 +102,7 @@ export const POST = async (request) => {
     // return new Response(JSON.stringify({ message: 'Success' }),
     //   { status: 200 });
   } catch (error) {
+    console.error("This is the error we are looking for: ", error);
     return new Response('Failed to add property', { status: 500 })
   }
 };
