81f4eb3cca23e8da7b18b42a09c4a7be35b90376
michaloumen <michelle.mendonca@minu.co>
2024-07-02 11:46:23 -0300

    user profile listings

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -11,7 +11,7 @@ export const GET = async (request) => {
 
     const properties = await Property.find({});
 
-    return new Response(JSON.stringify(properties));
+    return new Response(JSON.stringify(properties), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response('Something went wrong', { status: 500 });
