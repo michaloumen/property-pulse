7cc8e4098e040caf74eac9af9690458de8ca1ae7
michaloumen <michelle.mendonca@minu.co>
2024-07-02 12:41:16 -0300

    teast notifications

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 import '../assets/styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { ToastContainer } from 'react-toastify';
 import AuthProvider from '../components/AuthProvider';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -17,6 +19,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
