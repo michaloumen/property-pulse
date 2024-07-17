80257241cd00865d9e5349130db729b87dfc23d0
michaloumen <michelle.mendonca@minu.co>
2024-07-16 21:12:29 -0300

    saved properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -8,10 +8,10 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <div>
-      <section className='px-4 py-6'>
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
         {properties.length === 0 ? (
-          <p>No properties found</p>
+          <p>No Properties Found</p>
         ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {properties.map((property) => (
@@ -19,8 +19,8 @@ const PropertiesPage = async () => {
             ))}
           </div>
         )}
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
